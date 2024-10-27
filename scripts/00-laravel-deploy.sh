4fd543819d7972e42232747f640ad462fed0372d
unknown <datp1907@gmail.com>
2024-10-27 17:00:36 +0700

    deploy

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -8,6 +8,8 @@ php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
+echo "generate"
+php artisan key:generate
 
 echo "Checking database connection..."
 # Kiểm tra kết nối cơ sở dữ liệu và lưu lỗi vào biến
@@ -24,3 +26,9 @@ php artisan migrate --force
 
 echo "Seeding the database..."
 php artisan db:seed --class=DatabaseSeeder
+# DB_CONNECTION=mysql
+# DB_HOST=127.0.0.1
+# DB_PORT=3306
+# DB_DATABASE=ecomerce_php_mvc
+# DB_USERNAME=root
+# DB_PASSWORD="11111"
