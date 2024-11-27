bc066f5a7e14587f1b267d69839c4444aa1164eb
unknown <datp1907@gmail.com>
2024-11-27 18:12:55 +0700

    fix dockerfile

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
- 
+
 export default defineConfig({
     plugins: [
         laravel([
@@ -8,4 +8,11 @@ export default defineConfig({
             'resources/js/app.js',
         ]),
     ],
+    build: {
+        outDir: 'public/build', // Ensure the output directory is correct
+        manifest: true, // Ensure manifest is enabled
+        rollupOptions: {
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+        },
+    },
 });
