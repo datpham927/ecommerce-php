c82f9728369172885e9b6309cb5fd442c621e713
unknown <datp1907@gmail.com>
2024-11-29 09:02:59 +0700

    update code

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,33 +1,24 @@
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
-echo "Create app key"
-php artisan key:generate --force
-
 echo "Caching config..."
 php artisan config:cache
-php artisan view:clear
 
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Building assets with npm..."
-if ! npm run build; then
-    echo "NPM build failed!"
-    exit 1
-fi
-
 echo "Checking database connection..."
+# Kiểm tra kết nối cơ sở dữ liệu và lưu lỗi vào biến
 if php artisan migrate --pretend; then
     echo "Database connection is working."
 else
     echo "Database connection failed!"
+    # In ra thông báo lỗi trực tiếp từ stdout và stderr
     echo "Error: $(php artisan migrate --pretend 2>&1)"
     exit 1
 fi
-
 echo "Running migrations..."
 php artisan migrate --force
 
 echo "Seeding the database..."
-php artisan db:seed
+ php artisan db:seed
